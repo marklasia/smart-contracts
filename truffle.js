41182c703dd59bf9826d2d6f454241d5781d7dba
Cody Lamson <tovarishfin@gmail.com>
2018-05-19 15:16:36 +0200

    truffle config updated

--- truffle.js
+++ truffle.js
@@ -2,10 +2,9 @@ module.exports = {
   networks: {
     dev: {
       host: 'localhost',
-      port: 8545,
+      port: 9545,
       network_id: 4447,
-      gas: 8e6,
-      gasPrice: 5e9
+      gasPrice: 1e9
     },
     ropsten: {
       host: 'localhost',
