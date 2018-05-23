d5457b1109cd83ce25c818fc9b7fa6a2fb5f4124
Cody Lamson <tovarishfin@gmail.com>
2018-05-23 16:44:15 +0200

    initial implementation

--- contracts/PoaManager.sol
+++ contracts/PoaManager.sol
@@ -205,7 +205,7 @@ contract PoaManager is Ownable {
     private
     returns (address _proxyContract)
   {
-    _proxyContract = new Proxy(_target);
+    _proxyContract = new Proxy(_target, address(registry));
   }
 
   // Create a PoaToken contract with given parameters, and set active value to true
