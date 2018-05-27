948e01ba0740ab60e6be4df89bebe33d5837cf53
Cody Lamson <tovarishfin@gmail.com>
2018-05-27 05:48:11 +0200

    fix for proxy due to way addresses are now stored

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -63,12 +63,13 @@ contract Proxy {
   {
     address _addr = proxyRegistry(); // contract address to call
     bytes4 _sig = bytes4(keccak256("getContractAddress32(bytes32)")); // function signature we are using
-    bytes32 _name = bytes32("PoaManager"); // function argument we are using
+    string memory _name = "PoaManager"; // function argument we are using
+    bytes32 _name32 = keccak256(_name);
 
     assembly {
       let _call := mload(0x40) // set _call to free memory pointer
       mstore(_call, _sig) // store _sig at _call pointer
-      mstore(add(_call, 0x04), _name) // store _name32 at _call offset by 4 bytes for pre-existing _sig
+      mstore(add(_call, 0x04), _name32) // store _name32 at _call offset by 4 bytes for pre-existing _sig
       
       // staticcall(g, a, in, insize, out, outsize) => 0 on error 1 on success
       let success := staticcall(
