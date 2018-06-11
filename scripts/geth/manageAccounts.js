ad55fc562c80f402a57bdf95d63df4e496c06b82
Volkan Bilici <vbilici@gmail.com>
2018-06-11 13:54:21 +0200

    fix typos

--- scripts/geth/manageAccounts.js
+++ scripts/geth/manageAccounts.js
@@ -2,10 +2,15 @@
 /* eslint no-var:0 */
 /* eslint no-console:0 */
 
+var UNLOCK_DURATION_AS_SECONDS = 60 * 60
+
 for (var index = 0; index < personal.listAccounts.length; index++) {
   var acc = personal.listAccounts[index]
   console.log(acc)
-  personal.unlockAccount(acc, 'bb', 60 * 60)
+  personal.unlockAccount(acc, 'bb', UNLOCK_DURATION_AS_SECONDS)
 }
 
-console.log('Accounts unloocked ready!')
+console.log(
+  personal.listAccounts.length + ' accounts unlocked for ',
+  +UNLOCK_DURATION_AS_SECONDS / 60 + ' minutes !'
+)
