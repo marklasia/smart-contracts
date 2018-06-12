0e85be82be1993eed3c58edafb255b466d808591
Volkan Bilici <vbilici@gmail.com>
2018-06-12 17:39:01 +0200

    rename camel case to kebap case for geth scripts

--- scripts/geth/start-geth.sh
+++ scripts/geth/start-geth.sh
@@ -11,4 +11,4 @@ fi
 printf '%s\n' "init genesis block ($FULL_PATH)"
 /usr/local/bin/geth --datadir "./privateChainData/" init "./scripts/geth/genesis.json"
 printf '%s\n' "running geth..."
-/usr/local/bin/geth --datadir "./privateChainData/" --rpc --rpcapi eth,net,web3,personal,miner,debug,txpool  --rpccorsdomain "*" --preload "scripts/geth/manageAccounts.js,scripts/geth/autoMine.js"  --nodiscover --networkid 4447 --targetgaslimit 94000000 --gasprice 0 --maxpeers 0 console
+/usr/local/bin/geth --datadir "./privateChainData/" --rpc --rpcapi eth,net,web3,personal,miner,debug,txpool  --rpccorsdomain "*" --preload "scripts/geth/manage-accounts.js,scripts/geth/auto-mine.js"  --nodiscover --networkid 4447 --targetgaslimit 94000000 --gasprice 0 --maxpeers 0 console
