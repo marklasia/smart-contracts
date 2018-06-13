c75eea47fdb669d062d014280859c27e5aa7930d
Philip Paetz <philip@brickblock.io>
2018-06-13 11:13:33 +0200

    refactored `start-geth.sh` script to work on all OSes without any $PATH magic

--- scripts/geth/manage-accounts.js
+++ scripts/geth/manage-accounts.js
@@ -5,9 +5,9 @@
 var UNLOCK_DURATION_AS_SECONDS = 60 * 60
 
 for (var index = 0; index < personal.listAccounts.length; index++) {
-  var acc = personal.listAccounts[index]
-  console.log(acc)
-  personal.unlockAccount(acc, 'bb', UNLOCK_DURATION_AS_SECONDS)
+  var account = personal.listAccounts[index]
+  console.log(account)
+  personal.unlockAccount(account, 'bb', UNLOCK_DURATION_AS_SECONDS)
 }
 
 console.log(
--- scripts/geth/start-geth.sh
+++ scripts/geth/start-geth.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-DIRECTORY=`dirname $0`
-PATH='/privateChainData/geth'
-FULL_PATH=$DIRECTORY$PATH
+#
+# This script brings up a local geth blockchain to run tests against.
+# If old chain data exists in the data dir, it will delete it and
+# start with a clean state.
 
-if [ -d "./$PATH" ]; then
-    printf '%s\n' "Removing old chain data ($PATH)"
-    /bin/rm -rf "./$PATH"
+GETH_DATA_DIR='./private-chain-data'
+
+if [ -d "$GETH_DATA_DIR/geth" ]; then
+    printf '%s\n' "Removing old chain data from $GETH_DATA_DIR/geth"
+    /bin/rm -rf "$GETH_DATA_DIR/geth"
 fi
 
-printf '%s\n' "init genesis block ($FULL_PATH)"
-/usr/local/bin/geth --datadir "./privateChainData/" init "./scripts/geth/genesis.json"
+printf '%s\n' "init genesis block in $GETH_DATA_DIR/geth"
+geth --datadir "$GETH_DATA_DIR" init "./scripts/geth/genesis.json"
 printf '%s\n' "running geth..."
-/usr/local/bin/geth --datadir "./privateChainData/" --rpc --rpcapi eth,net,web3,personal,miner,debug,txpool  --rpccorsdomain "*" --preload "scripts/geth/manage-accounts.js,scripts/geth/auto-mine.js"  --nodiscover --networkid 4447 --targetgaslimit 94000000 --gasprice 0 --maxpeers 0 console
+geth --datadir "$GETH_DATA_DIR" \
+  --gasprice 0 \
+  --maxpeers 0 console \
+  --networkid 4447 \
+  --nodiscover \
+  --preload "scripts/geth/manage-accounts.js,scripts/geth/auto-mine.js" \
+  --rpc \
+  --rpcapi eth,net,web3,personal,miner,debug,txpool \
+  --rpccorsdomain "*" \
+  --targetgaslimit 94000000
